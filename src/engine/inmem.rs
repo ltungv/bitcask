6ebd69e6ae8f664f2aa343eb280962a9ec797964
Tung Le Vo <2381764-letung3105@noreply.gitlab.com>
2022-03-29 16:55:12 +0700

    use DashMap

--- src/engine/inmem.rs
+++ src/engine/inmem.rs
@@ -1,23 +1,20 @@
 use super::KeyValueStore;
 use crate::error::{Error, ErrorKind};
 use bytes::Bytes;
-use std::{
-    collections::HashMap,
-    sync::{Arc, Mutex},
-};
+use dashmap::DashMap;
+use std::sync::Arc;
 
 /// A type alias for a our database type
 #[derive(Default)]
 pub struct InMemoryStorage {
-    inner: Arc<Mutex<HashMap<String, Bytes>>>,
+    inner: Arc<DashMap<String, Bytes>>,
 }
 
 impl KeyValueStore for InMemoryStorage {
     /// Delete a key from the store. Returns the value of the removed
     /// key, if there's any
     fn del(&self, key: &str) -> Result<(), Error> {
-        let mut map = self.inner.lock().unwrap();
-        match map.remove(key) {
+        match self.inner.remove(key) {
             Some(_) => Ok(()),
             None => Err(Error::from(ErrorKind::KeyNotFound)),
         }
@@ -25,8 +22,7 @@ impl KeyValueStore for InMemoryStorage {
 
     /// Get the value of a key from the store.
     fn get(&self, key: &str) -> Result<Bytes, Error> {
-        let map = self.inner.lock().unwrap();
-        match map.get(key) {
+        match self.inner.get(key) {
             Some(v) => Ok(v.clone()),
             None => Err(Error::from(ErrorKind::KeyNotFound)),
         }
@@ -35,8 +31,7 @@ impl KeyValueStore for InMemoryStorage {
     /// Sets the value to a key. Returns the previous value of the key,
     /// if there's any.
     fn set(&self, key: String, value: Bytes) -> Result<(), Error> {
-        let mut map = self.inner.lock().unwrap();
-        map.insert(key, value);
+        self.inner.insert(key, value);
         Ok(())
     }
 }
