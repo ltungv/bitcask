09ea20f38f18ded509fa06f7ef94051617612d76
Tung Le Vo <2381764-letung3105@noreply.gitlab.com>
2022-04-02 22:06:02 +0700

    fix log name parsing

--- src/engine/bitcask/utils.rs
+++ src/engine/bitcask/utils.rs
@@ -1,15 +1,15 @@
 use std::{collections::BTreeSet, ffi::OsStr, fs, io, path::Path, time};
 
-const DATAFILE_EXT: &str = "bitcask.data";
+const DATAFILE_EXT: &str = "data";
 
-const HINTFILE_EXT: &str = "bitcask.hint";
+const HINTFILE_EXT: &str = "hint";
 
 pub fn datafile_name(fileid: u64) -> String {
-    format!("{}.{}", fileid, DATAFILE_EXT)
+    format!("{}.bitcask.{}", fileid, DATAFILE_EXT)
 }
 
 pub fn hintfile_name(fileid: u64) -> String {
-    format!("{}.{}", fileid, HINTFILE_EXT)
+    format!("{}.bitcask.{}", fileid, HINTFILE_EXT)
 }
 
 pub fn sorted_fileids<P>(path: P) -> io::Result<Vec<u64>>
@@ -22,10 +22,15 @@ where
         .filter_map(std::result::Result::ok)
         // extract paths
         .map(|e| e.path())
-        // get files with data file extentions
+        // get files with data file extensions
         .filter(|p| p.is_file() && p.extension() == Some(OsStr::new(DATAFILE_EXT)))
         // parse the file id as u64
-        .filter_map(|p| p.file_stem().and_then(OsStr::to_str).map(str::parse::<u64>))
+        .filter_map(|p| {
+            p.file_stem()
+                .and_then(OsStr::to_str)
+                .and_then(|s| s.split('.').next())
+                .map(str::parse::<u64>)
+        })
         .filter_map(std::result::Result::ok)
         .collect();
 
