010566676d97a651f256ed9723e98b4bb0625994
Tung Le Vo <2381764-letung3105@noreply.gitlab.com>
2022-04-03 21:00:57 +0700

    refactor benches

--- benches/compare_engines_sequential.rs
+++ benches/compare_engines_sequential.rs
@@ -95,20 +95,20 @@ pub fn bench_read(c: &mut Criterion) {
 
     {
         let (engine, _tmpdir) = get_bitcask();
-        g.bench_with_input("bitcask", &(engine, &kv_pairs), sequential_read_bulk_bench);
+        g.bench_with_input("bitcask", &(&kv_pairs, engine), sequential_read_bulk_bench);
     }
     {
         let (engine, _tmpdir) = get_sled();
-        g.bench_with_input("sled", &(engine, &kv_pairs), sequential_read_bulk_bench);
+        g.bench_with_input("sled", &(&kv_pairs, engine), sequential_read_bulk_bench);
     }
     {
         let (engine, _tmpdir) = get_dashmap();
-        g.bench_with_input("dashmap", &(engine, &kv_pairs), sequential_read_bulk_bench);
+        g.bench_with_input("dashmap", &(&kv_pairs, engine), sequential_read_bulk_bench);
     }
     g.finish();
 }
 
-fn sequential_read_bulk_bench<E>(b: &mut Bencher, (engine, kv_pairs): &(E, &Vec<KeyValuePair>))
+fn sequential_read_bulk_bench<E>(b: &mut Bencher, (kv_pairs, engine): &(&Vec<KeyValuePair>, E))
 where
     E: KeyValueStore,
 {
