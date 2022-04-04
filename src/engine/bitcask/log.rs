ff82e3b73326150543971a034286d018f0d5e2e6
Tung Le Vo <2381764-letung3105@noreply.gitlab.com>
2022-04-05 00:41:15 +0700

    persist Mmap object

--- src/engine/bitcask/log.rs
+++ src/engine/bitcask/log.rs
@@ -52,15 +52,16 @@ impl LogDir {
 
     /// Return the reader of the file with the given `fileid`. If there's no reader for the file
     /// with the given `fileid`, create a new reader and return it.
-    pub fn get<P>(&mut self, path: P, fileid: u64) -> io::Result<&LogReader>
+    pub fn get<P>(&mut self, path: P, fileid: u64) -> io::Result<&mut LogReader>
     where
         P: AsRef<Path>,
     {
         if !self.0.contains_key(&fileid) {
-            let reader = LogReader::new(open(utils::datafile_name(&path, fileid))?);
+            let file = open(utils::datafile_name(&path, fileid))?;
+            let reader = LogReader::new(file)?;
             Ok(self.0.entry(fileid).or_insert(reader))
         } else {
-            Ok(self.0.get(&fileid).expect("unreachable error"))
+            Ok(self.0.get_mut(&fileid).expect("unreachable error"))
         }
     }
 
@@ -118,12 +119,16 @@ impl LogWriter {
 
 /// A random-access file reader that deserializes data using `bincode`.
 #[derive(Debug)]
-pub struct LogReader(fs::File);
+pub struct LogReader {
+    mmap: memmap2::Mmap,
+    file: fs::File,
+}
 
 impl LogReader {
     /// Create a new log reader for reading entries from the given file.
-    pub fn new(file: fs::File) -> Self {
-        Self(file)
+    pub fn new(file: fs::File) -> io::Result<Self> {
+        let mmap = unsafe { memmap2::MmapOptions::new().map(&file)? };
+        Ok(Self { mmap, file })
     }
 
     /// Return the entry at the given position by mapping the file segment directly into memory.
@@ -131,16 +136,16 @@ impl LogReader {
     /// # Unsafe
     ///
     /// The caller must ensure that the file segment given by `len` and `pos` is valid.
-    pub unsafe fn at<T>(&self, len: u64, pos: u64) -> bincode::Result<T>
+    pub unsafe fn at<T>(&mut self, len: u64, pos: u64) -> bincode::Result<T>
     where
         T: DeserializeOwned,
     {
-        let mmap = memmap2::MmapOptions::new()
-            .offset(pos)
-            .len(len as usize)
-            // NOTE: we convert an io::Error into bincode::Error which might be undesirable
-            .map(&self.0)?;
-        bincode::deserialize(&mmap)
+        if pos >= self.mmap.len() as u64 {
+            self.mmap = memmap2::MmapOptions::new().map(&self.file)?;
+        }
+        let start = pos as usize;
+        let end = start + len as usize;
+        bincode::deserialize(&self.mmap[(start..end)])
     }
 
     /// Copy the raw data at the given position into the writer at `dst` by mapping the file segment
@@ -149,15 +154,16 @@ impl LogReader {
     /// # Unsafe
     ///
     /// The caller must ensure that the file segment given by `len` and `pos` is valid.
-    pub unsafe fn copy_raw<W>(&self, len: u64, pos: u64, dst: &mut W) -> io::Result<u64>
+    pub unsafe fn copy_raw<W>(&mut self, len: u64, pos: u64, dst: &mut W) -> io::Result<u64>
     where
         W: Write,
     {
-        let mmap = memmap2::MmapOptions::new()
-            .offset(pos)
-            .len(len as usize)
-            .map(&self.0)?;
-        io::copy(&mut mmap.reader(), dst)
+        if pos >= self.mmap.len() as u64 {
+            self.mmap = memmap2::MmapOptions::new().map(&self.file)?;
+        }
+        let start = pos as usize;
+        let end = start + len as usize;
+        io::copy(&mut self.mmap[start..end].reader(), dst)
     }
 }
 
@@ -224,11 +230,36 @@ mod tests {
             let idx1 = writer.append(&buf).unwrap();
             let idx2 = writer.append(&buf).unwrap();
             // read the entry
-            let reader = LogReader::new(open(&fpath).unwrap());
+            let mut reader = LogReader::new(open(&fpath).unwrap()).unwrap();
             let buf1 = unsafe { reader.at::<Vec<u8>>(idx1.len, idx1.pos).unwrap() };
             let buf2 = unsafe { reader.at::<Vec<u8>>(idx2.len, idx2.pos).unwrap() };
             // succeed if we received the correct data given the positions
             buf == buf1 && buf == buf2
+        }
+    }
+    quickcheck! {
+        fn reader_should_remap_disk_when_file_changed(buf: Vec<u8>) -> bool {
+            let dir = tempfile::tempdir().unwrap();
+            let fpath = dir.as_ref().join("test");
+            let mut writer = LogWriter::new(create(&fpath).unwrap()).unwrap();
+            let mut reader = LogReader::new(open(&fpath).unwrap()).unwrap();
+
+            // write the entry
+            let idx1 = writer.append(&buf).unwrap();
+            let idx2 = writer.append(&buf).unwrap();
+            // read the entry (should remap)
+            let buf1 = unsafe { reader.at::<Vec<u8>>(idx1.len, idx1.pos).unwrap() };
+            let buf2 = unsafe { reader.at::<Vec<u8>>(idx2.len, idx2.pos).unwrap() };
+
+            // write some more
+            let idx3 = writer.append(&buf).unwrap();
+            let idx4 = writer.append(&buf).unwrap();
+            // read the entry (should remap)
+            let buf3 = unsafe { reader.at::<Vec<u8>>(idx3.len, idx3.pos).unwrap() };
+            let buf4 = unsafe { reader.at::<Vec<u8>>(idx4.len, idx4.pos).unwrap() };
+
+            // succeed if we received the correct data given the positions
+            buf == buf1 && buf == buf2 && buf == buf3 && buf == buf4
         }
     }
 
@@ -240,7 +271,7 @@ mod tests {
             let mut writer = LogWriter::new(create(&fpath).unwrap()).unwrap();
             let indices: Vec<LogIndex> = entries.iter().map(|buf| writer.append(&buf).unwrap()).collect();
             // read the entries
-            let reader = LogReader::new(open(&fpath).unwrap());
+            let mut reader = LogReader::new(open(&fpath).unwrap()).unwrap();
             let mut iter = LogIterator::new(open(&fpath).unwrap()).unwrap();
             for (idx, buf) in indices.iter().zip(entries) {
                 let (idx_from_reader, buf_from_reader) = iter.next::<Vec<u8>>().unwrap().unwrap();
