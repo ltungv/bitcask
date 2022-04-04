6feaf16e72239dd59e10cf3b7760cd8812b07b86
Tung Le Vo <2381764-letung3105@noreply.gitlab.com>
2022-04-04 12:05:26 +0700

    combine benches

--- benches/compare_engines.rs
+++ benches/compare_engines.rs
@@ -2,7 +2,8 @@ mod common;
 
 use common::{
     concurrent_read_bulk_bench_iter, concurrent_write_bulk_bench_iter, get_bitcask, get_dashmap,
-    get_sled, get_threadpool, rand_kv_pairs, KeyValuePair,
+    get_sled, get_threadpool, rand_kv_pairs, sequential_read_bulk_bench_iter,
+    sequential_write_bulk_bench_iter, KeyValuePair,
 };
 use criterion::{criterion_group, criterion_main, BatchSize, Bencher, Criterion, Throughput};
 use opal::engine::{self, KeyValueStore};
@@ -10,8 +11,8 @@ use pprof::criterion::{Output, PProfProfiler};
 use rand::prelude::*;
 
 const ITER: usize = 1000;
-const KEY_SIZE: usize = 1000;
-const VAL_SIZE: usize = 1000;
+const KEY_SIZE: usize = 512;
+const VAL_SIZE: usize = 1024;
 
 pub fn bench_write(c: &mut Criterion) {
     let kv_pairs = rand_kv_pairs(ITER, KEY_SIZE, VAL_SIZE);
@@ -20,23 +21,45 @@ pub fn bench_write(c: &mut Criterion) {
         nbytes += k.len() + v.len();
     }
 
-    let mut g = c.benchmark_group("compare_engines_concurrent_write");
+    let mut g = c.benchmark_group("bitcask");
     g.throughput(Throughput::Bytes(nbytes as u64));
-
     g.bench_with_input(
-        "bitcask",
+        "concurrent",
         &(&kv_pairs, engine::Type::BitCask),
         concurrent_write_bulk_bench,
     );
     g.bench_with_input(
-        "sled",
+        "sequential",
+        &(&kv_pairs, engine::Type::BitCask),
+        sequential_write_bulk_bench,
+    );
+    g.finish();
+
+    let mut g = c.benchmark_group("sled");
+    g.throughput(Throughput::Bytes(nbytes as u64));
+    g.bench_with_input(
+        "concurrent",
         &(&kv_pairs, engine::Type::Sled),
+        concurrent_write_bulk_bench,
+    );
+    g.bench_with_input(
+        "sequential",
+        &(&kv_pairs, engine::Type::Sled),
+        sequential_write_bulk_bench,
+    );
+    g.finish();
+
+    let mut g = c.benchmark_group("dashmap");
+    g.throughput(Throughput::Bytes(nbytes as u64));
+    g.bench_with_input(
+        "concurrent",
+        &(&kv_pairs, engine::Type::DashMap),
         concurrent_write_bulk_bench,
     );
     g.bench_with_input(
         "dashmap",
         &(&kv_pairs, engine::Type::DashMap),
-        concurrent_write_bulk_bench,
+        sequential_write_bulk_bench,
     );
     g.finish();
 }
@@ -86,6 +109,44 @@ fn concurrent_write_bulk_bench(
     }
 }
 
+fn sequential_write_bulk_bench(
+    b: &mut Bencher,
+    (kv_pairs, engine): &(&Vec<KeyValuePair>, engine::Type),
+) {
+    match *engine {
+        engine::Type::BitCask => {
+            b.iter_batched(
+                || {
+                    let (engine, tmpdir) = get_bitcask();
+                    (engine, kv_pairs.to_vec(), tmpdir)
+                },
+                sequential_write_bulk_bench_iter,
+                BatchSize::SmallInput,
+            );
+        }
+        engine::Type::Sled => {
+            b.iter_batched(
+                || {
+                    let (engine, tmpdir) = get_sled();
+                    (engine, kv_pairs.to_vec(), tmpdir)
+                },
+                sequential_write_bulk_bench_iter,
+                BatchSize::SmallInput,
+            );
+        }
+        engine::Type::DashMap => {
+            b.iter_batched(
+                || {
+                    let (engine, tmpdir) = get_dashmap();
+                    (engine, kv_pairs.to_vec(), tmpdir)
+                },
+                sequential_write_bulk_bench_iter,
+                BatchSize::SmallInput,
+            );
+        }
+    }
+}
+
 pub fn bench_read(c: &mut Criterion) {
     let kv_pairs = rand_kv_pairs(ITER, KEY_SIZE, VAL_SIZE);
     let mut nbytes = 0;
@@ -93,24 +154,55 @@ pub fn bench_read(c: &mut Criterion) {
         nbytes += k.len() + v.len();
     }
 
-    let mut g = c.benchmark_group("compare_engines_concurrent_read");
+    let mut g = c.benchmark_group("bitcask");
     g.throughput(Throughput::Bytes(nbytes as u64));
-
     g.bench_with_input(
-        "bitcask",
+        "concurrent",
         &(&kv_pairs, engine::Type::BitCask),
         concurrent_read_bulk_bench,
     );
+    {
+        let (engine, _tmpdir) = get_bitcask();
+        g.bench_with_input(
+            "sequential",
+            &(&kv_pairs, engine),
+            sequential_read_bulk_bench,
+        );
+    }
+    g.finish();
+
+    let mut g = c.benchmark_group("sled");
+    g.throughput(Throughput::Bytes(nbytes as u64));
     g.bench_with_input(
-        "sled",
+        "concurrent",
         &(&kv_pairs, engine::Type::Sled),
         concurrent_read_bulk_bench,
     );
+    {
+        let (engine, _tmpdir) = get_sled();
+        g.bench_with_input(
+            "sequential",
+            &(&kv_pairs, engine),
+            sequential_read_bulk_bench,
+        );
+    }
+    g.finish();
+
+    let mut g = c.benchmark_group("dashmap");
+    g.throughput(Throughput::Bytes(nbytes as u64));
     g.bench_with_input(
-        "dashmap",
+        "concurrent",
         &(&kv_pairs, engine::Type::DashMap),
         concurrent_read_bulk_bench,
     );
+    {
+        let (engine, _tmpdir) = get_dashmap();
+        g.bench_with_input(
+            "sequential",
+            &(&kv_pairs, engine),
+            sequential_read_bulk_bench,
+        );
+    }
     g.finish();
 }
 
@@ -173,6 +265,25 @@ fn concurrent_read_bulk_bench(
             });
         }
     }
+}
+
+fn sequential_read_bulk_bench<E>(b: &mut Bencher, (kv_pairs, engine): &(&Vec<KeyValuePair>, E))
+where
+    E: KeyValueStore,
+{
+    kv_pairs.iter().cloned().for_each(|(k, v)| {
+        engine.set(k, v).unwrap();
+    });
+
+    b.iter_batched(
+        || {
+            let mut kv_pairs = kv_pairs.to_vec();
+            kv_pairs.shuffle(&mut rand::thread_rng());
+            (engine.clone(), kv_pairs)
+        },
+        sequential_read_bulk_bench_iter,
+        BatchSize::SmallInput,
+    );
 }
 
 criterion_group!(
